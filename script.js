4c4ca4364d9bfa5874ccb35c7041cf9cd776da28
Alice Fung <alice.o.fung@gmail.com>
2015-09-04 15:05:06 -0400

    working on refresher method

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var PUPPY = PUPPY || {}
+var PUPPY = PUPPY || {};
 
 PUPPY.ajaxRefresher = (function(){
 
@@ -9,22 +9,37 @@ PUPPY.ajaxRefresher = (function(){
       dataType: "json",
       async: true,
     });
+
     var info = getPuppyInfo($puppies.responseJSON);
     for (keys in info) {
-      
-    };
-    $("#puppies-list")
-  }
+      $("#puppies-list").append('<li><b>'+key+'</b>'+ ' (' +
+                          info[key][breed] + '), created ' +
+                          Date.new(Date.parse(info[key][found])) +
+                          '--' + '<a href='+ info[key][url] +'>'+
+                          Adopt + '</a></li>');
 
-  var getPuppyInfo = function (jsonObj) {
+    }
 
-  }
+    return info;
+  };
+
+  var getPuppyInfo = function (jsonArr) {
+    var filteredInfo = {};
+    for(var i=0; i < jsonArr.length; i++){
+      filteredInfo[jsonArr[i].name]= { breed: jsonArr[i].breed.name,
+                                      found: jsonArr[i].created_at,
+                                      url: jsonArr[i].url };
+    }
+    return filteredInfo;
+  };
 
   return {
-
-  }
+    getPuppiesList: getPuppiesList
+  };
 })();
 
 $(document).ready(function(){
-
+  PUPPY.ajaxRefresher.getPuppiesList();
 });
+
+
