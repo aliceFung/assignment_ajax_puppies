dedc238ae5d2b4385ba57ffa3b25029f39e65d71
Alice Fung <alice.o.fung@gmail.com>
2015-09-04 18:35:14 -0400

    before post works

--- script.js
+++ script.js
@@ -30,8 +30,8 @@ PUPPY.ajaxRefresher = (function () {
   };
 
   var alertUser = function(xhr, status, errorThrown) {
-    $("#flash").removeClass("hidden").text(status + ": " + errorThrown)
-  }
+    $("#flash").removeClass("hidden alert-success").addClass('alert-warning').text(status + ": " + errorThrown);
+  };
 
   var showListOfPuppies = function(json) {
     var info = getPuppyInfo(json);
@@ -55,14 +55,83 @@ PUPPY.ajaxRefresher = (function () {
     return filteredInfo;
   };
 
+  var getBreeds = function(){
+    $.ajax({
+      method: "GET",
+      url: "https://pacific-stream-9205.herokuapp.com/breeds.json",
+      dataType: "json",
+      contentType: "application/json",
+      // header: { 'Access-Control-Allow-Origin': 'http://localhost:3000'},
+      async: true,
+      success: function(jsonArr) {
+        buildDropDownMenu(jsonArr);
+      },
+
+      error: function(xhr, status, errorThrown) {
+        alertUser(xhr, status, errorThrown);
+      },
+
+      complete: function(xhr) {
+        console.log("breeds request complete");
+      }
+
+    });
+  };
+
+  var buildDropDownMenu = function(jsonArr){
+    var $breed = $('#breed');
+    for (var i=0; i< jsonArr.length; i++){
+      $breed.append('<option value="' + jsonArr[i].id + '">'+
+                  jsonArr[i].name + '</option>');
+    }
+  };
+
+  var registerPuppy = function(event){
+    event.preventDefault();
+    var puppyName = $('#form input').val();
+    var breedVal = $('#form select').val();
+    $.ajax({
+      method: "POST",
+      url: "https://pacific-stream-9205.herokuapp.com/puppies.json",
+      data: {breed_id: breedVal, name: puppyName},
+      dataType: "json",
+      contentType: "application/json",
+      headers: { 'Access-Control-Allow-Origin': '*'},
+      type: "POST",
+      async: true,
+      success: function(jsonArr) {
+        showSuccessAlert('Puppy is ready for adoption');
+        addNewPuppy(jsonArr);
+      },
+
+      error: function(xhr, status, errorThrown) {
+        alertUser(xhr, status, errorThrown);
+      },
+
+      complete: function(xhr) {
+        console.log("breeds request complete");
+      }
+
+    });
+  };
+
+  var showSuccessAlert = function(message){
+    $('#flash').removeClass('hidden alert-warning').addClass('alert-success').text(message);
+  };
+
   var setListener = function () {
     $("body").on("click", "#refresh", function(event) {
       getPuppiesList(event);
     });
-  }
+    $('body').on('click', '#register', function(event){
+      registerPuppy(event);
+    });
+  };
 
   return {
     getPuppiesList: getPuppiesList,
+    getBreeds: getBreeds,
+    registerPuppy: registerPuppy,
     setListener: setListener,
   };
 })();
@@ -70,6 +139,7 @@ PUPPY.ajaxRefresher = (function () {
 $(document).ready(function(){
   PUPPY.ajaxRefresher.setListener();
   PUPPY.ajaxRefresher.getPuppiesList();
+  PUPPY.ajaxRefresher.getBreeds();
 });
 
 
